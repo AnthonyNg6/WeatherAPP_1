d6783f2c77582e217ff9f462902c74388115a531
AnthonyNg6 <134814209+AnthonyNg6@users.noreply.github.com>
2023-06-16 16:11:29 -0700

    Updated Main
    Added API

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-#!/usr/bin/env python3
-"""
-Module Docstring
-"""
+import requests
+from tkinter import *
+import math
 
-__author__ = "Your Name"
-__version__ = "0.1.0"
-__license__ = "MIT"
+API_KEY = "3ace7596caad5c7cebbc0e883eeb0fef"
+city_name = input("Enter City: ")
 
+def get_weather(api_key, city):
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
 
-def main():
-    """ Main entry point of the app """
-    print("hello world")
+    response = requests.get(url).json()
+    temp = response['main']['temp']
+    temp = math.floor((temp - 273.15)*1.8)+32
 
+    feels_like = response['main']['feels_like']
+    feels_like = math.floor((feels_like - 273.15)*1.8)+32
 
-if __name__ == "__main__":
-    """ This is executed when run from the command line """
-    main()
+    humidity = response['main']['humidity']
+
+    print("Temperature: " + str(temp) + "°F")
+    print("Feels like: " + str(feels_like) + "°F")
+    print("Humidity: " + str(humidity) + "%")
+
+get_weather(API_KEY,city_name)
